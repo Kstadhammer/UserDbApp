be83053bb9e59d4380f0a30e0e5656628060399d
Rubbeman <109215347+Kstadhammer@users.noreply.github.com>
2024-12-19 16:59:05 +0100

    Update ViewModels and MainWindow view

--- UserDataBaseApp.Avalonia/ViewModels/UserViewModel.cs
+++ UserDataBaseApp.Avalonia/ViewModels/UserViewModel.cs
@@ -1,6 +1,53 @@
+using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using UserDataBaseApp.Avalonia.Models;
+
 namespace UserDataBaseApp.Avalonia.ViewModels;
 
-public class UserViewModel
+public partial class UserViewModel : ViewModelBase
 {
-    
+    [ObservableProperty]
+    private User _user;
+
+    [ObservableProperty]
+    private string _firstName;
+
+    [ObservableProperty]
+    private string _lastName;
+
+    [ObservableProperty]
+    private string _email;
+
+    [ObservableProperty]
+    private string _phoneNumber;
+
+    [ObservableProperty]
+    private string _address;
+
+    [ObservableProperty]
+    private string _postalCode;
+
+    [ObservableProperty]
+    private string _city;
+
+    public UserViewModel(User user)
+    {
+        User = user;
+    }
+
+    public User GetUser()
+    {
+        return new User
+        {
+            Id = Guid.NewGuid().ToString(),
+            TimeCreated = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+            FirstName = User.FirstName,
+            LastName = User.LastName,
+            Email = User.Email,
+            PhoneNumber = User.PhoneNumber,
+            Address = User.Address,
+            PostalCode = User.PostalCode,
+            City = User.City,
+        };
+    }
 }
--- UserDataBaseApp.Avalonia/ViewModels/ViewModelBase.cs
+++ UserDataBaseApp.Avalonia/ViewModels/ViewModelBase.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using UserDataBaseApp.Avalonia.Models;
 
 namespace UserDataBaseApp.Avalonia.ViewModels;
 
-public class ViewModelBase : ObservableObject
-{
-}
+public class ViewModelBase : ObservableObject { }
